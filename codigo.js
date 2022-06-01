477698f67ef90e1d19d092e3f58ac4270e1178d6
Estefania Cufre <estefaniacufre@gmail.com>
2022-05-31 23:22:39 -0300

    desafio complementario Arrays

--- codigo.js
+++ codigo.js
@@ -1,4 +1,4 @@
-let inicio = prompt("¡Bienvenido a la tienda virtual de CianoTef! ¿Querés adquirir obras originales?")
+/* let inicio = prompt("¡Bienvenido a la tienda virtual de CianoTef! ¿Querés adquirir obras originales?")
 let productos;
 let sumatoria=0;
 let precio=2000
@@ -23,4 +23,79 @@ else {
     alert ("¡Ok! ¡Hasta la próxima!")
 };
 
+*/
 
+const misObras=[
+    {
+        titulo:"Su nombre no importa",
+        serie:"Ciudades",
+        papel: "Acuarela",
+        precio:2000
+    },
+    {
+        titulo:"Palacio de Comunicaciones",
+        serie:"Ciudades",
+        papel: "Acuarela",
+        precio:2200
+    },
+    {
+        titulo:"Serpiente metálica",
+        serie:"Ciudades",
+        papel: "Acuarela",
+        precio:2500
+    },
+    {
+        titulo:"La Castellana",
+        serie:"Ciudades",
+        papel: "Vegetal",
+        precio:2000
+    },
+    {
+        titulo:"Mensajes aéreos",
+        serie:"Silencios territoriales",
+        papel: "Vegetal",
+        precio:2200
+    },
+   {
+        titulo:"¿Sueñan los monjes con feriados?",
+        serie:"Silencios territoriales",
+        papel: "Acuarela",
+        precio:2500
+    },
+    {
+        titulo:"Desde afuera",
+        serie:"Silencios territoriales",
+        papel: "Acuarela",
+        precio:2000
+    },
+    {
+        titulo:"Novios ausentes",
+        serie:"Silencios territoriales",
+        papel: "Vegetal",
+        precio:2500
+    },
+];
+
+for(const obra of misObras){
+    console.log("Titulo: "+obra.titulo+" - Precio: $"+obra.precio);
+}
+
+
+class Obra{
+    constructor(misObras){
+        this.titulo=misObras.titulo;
+        this.serie=misObras.serie;
+        this.papel=misObras.papel;
+        this.precio=misObras.precio;
+        
+    }
+    sumaIVA(){
+        this.precio=this.precio*1.21;
+    }   
+}
+
+for (const obra of misObras) {
+misObras.push(new Obra(misObras.titulo="El movimiento estático",misObras.serie="Silencios territoriales", misObras.papel="Acuarela", misObras.precio=2250))
+}
+
+console.log(misObras);
